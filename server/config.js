5b433ecf9732efca6ea14bf8c2d0797ca3e71e7a
elvaigh <elvaigh@gmail.com>
2025-07-09 18:07:51 +0200

    first commit

--- server/config.js
+++ server/config.js
@@ -1,7 +1,8 @@
+
 module.exports = {
-    DB_USER: 'celvaigh',
-    DB_HOST: 'localhost',
-    DB_NAME: 'iziexplorer',
-    DB_PASSWORD: 'izi',
-    DB_PORT: 5432,
-  };
+  DB_USER: 'celvaigh',
+  DB_HOST: 'dpg-d1n93vfdiees73eujmu0-a',
+  DB_NAME: 'iziexplorerdb',
+  DB_PASSWORD: '2nxDD3ssEtAjxK3zRcd52g8lGTy97OZW',
+  DB_PORT: 5432,
+};
